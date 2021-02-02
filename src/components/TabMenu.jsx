f5ab5ac6ba1d8f6c8ed763493f9d5d055100cb5f
Cameron Brouk <cameronbrouk@gmail.com>
2021-02-02 16:17:59 -0600

    Code formatting changes within TabMenu

--- src/components/TabMenu.jsx
+++ src/components/TabMenu.jsx
@@ -1,36 +1,14 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Tab from 'components/Tab'
 
 import 'stylesheets/TabMenu.scss'
 
-const TabMenu = props => {
+const TabMenu = ({ tabs }) => {
   const [activeTab, setActiveTab] = useState('gender')
-  /**
-   * Render list of clickable tabs
-   */
-  const renderTabs = () => {
-    if (!props.tabs) return null
-    return (
-      <ul className='tabList'>
-        {props.tabs.map(tab => (
-          <li key={`tab-${tab.id}`}>
-            <Tab
-              label={tab.label}
-              id={tab.id}
-              onClickAction={() => setActiveTab(tab.id)}
-              active={tab.id === activeTab}
-            />
-          </li>
-        ))}
-      </ul>
-    )
-  }
 
-  /**
-   * Render content of currently active tab only
-   */
+  // Render content of currently active tab only
   const renderActiveContent = () => {
-    const currentTab = props.tabs.find(({ id }) => id === activeTab)
+    const currentTab = tabs.find(({ id }) => id === activeTab)
     if (!currentTab) return null
     if (!currentTab.content) return null
     return <div className='content'>{currentTab.content}</div>
@@ -38,7 +16,20 @@ const TabMenu = props => {
 
   return (
     <div className='tabMenu'>
-      {renderTabs()}
+      {tabs && (
+        <ul className='tabList'>
+          {tabs.map(tab => (
+            <li key={`tab-${tab.id}`}>
+              <Tab
+                label={tab.label}
+                id={tab.id}
+                onClickAction={() => setActiveTab(tab.id)}
+                active={tab.id === activeTab}
+              />
+            </li>
+          ))}
+        </ul>
+      )}
       {renderActiveContent()}
     </div>
   )
